b132b89ed7b9a65debcdf2b93be22cd6bd4b9796
xerofoodychefs-droid <xerofoodychefs@gmail.com>
2026-08-27 04:17:52 +0530

    Test backend auto deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,3 +38,5 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+
+# Deployment pipeline test
